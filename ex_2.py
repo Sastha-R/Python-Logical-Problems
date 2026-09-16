8e7824f2a60a4840d9ae4f032621372af8258624
Sastha <sastha00007@gmail.com>
2026-09-16 10:38:19 +0530

    2 problems added

---
+++ ex_2.py
@@ -0,0 +1,16 @@
+amount = 50
+
+print("500 : " , amount // 500)
+
+rem = amount % 500
+
+print("200 : ",rem // 200)
+
+rem = rem % 200
+
+print("100 : " , rem // 100)
+
+rem = rem % 100
+
+print("50 : ",rem // 50)
+
---
+++ ex_3.py
@@ -0,0 +1,11 @@
+sec = 4530
+
+print(sec // 3600 , "hour")
+
+rem = sec % 3600
+
+print(rem // 60 , "minutes")
+
+rem = rem % 60 
+
+print(rem,"seconds")
